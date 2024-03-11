2916163248fb6b4622e45d0814ef7ea93eddebc3
awear321 <155543391+awear321@users.noreply.github.com>
2024-03-11 21:34:56 +0900

    Delete super-coding directory

--- super-coding/src/main/java/com/github/supercodingspring/repository/AirlineTicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.supercodingspring.repository.AirlineTicket;
-
-import java.util.List;
-
-public interface AirlineTicketRepository {
-    List<AirlineTicket> findAllAirlineTicketsWithPlaceAndTicketType(String likePlace, String ticketType);
-
-    List<AirlineTicketAndFlightInfo> findAllAirLineTicketAndFlightInfo(Integer airlineTicketId);
-}
